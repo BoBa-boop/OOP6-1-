47a54619f66547b415ef22ece80582b1d3245afc
Никита Федосеев <zveri132@gmail.com>
2020-06-22 15:53:02 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Num
+    {
+        public int _num1;
+        public int _num2;
+        public Num()
+        {
+
+        }
+        public Num(int num1)
+        {
+            _num1 = num1;
+        }
+        public Num(int num1, int num2)
+        {
+            _num1 = num1;
+            _num2 = num2;
+        }
+        public void Display()
+        {
+            Console.WriteLine(_num1+" "+_num2);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Num num = new Num();
+            num.Display();
+            Num num_1 = new Num(5);
+            num_1.Display();
+            Num num_2 = new Num(8, 7);
+            num_2.Display();
+            Console.ReadKey();
+        }
+    }
+}
